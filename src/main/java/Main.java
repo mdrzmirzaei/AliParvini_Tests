e94cafb040525978cd68e943498d1a2ede510399
mohammadreza mirzaei <mdrz.mirzaei@gmail.com>
2023-05-17 11:25:39 +0330

    stream

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("نام محن محمدرضا ست");
         //11111111111111111111
 //        TDD tdd = new TDD();
 //        System.out.println(tdd.date());
